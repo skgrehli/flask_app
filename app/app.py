3248ca88c11f935d9715f83fcf75a450c14d18cd
Rickard Samuelsson <rickardsamuelsson@Rickards-MacBook-Pro.local>
2019-05-13 16:33:15 +0200

    Testing if app in subfolder

--- app/app.py
+++ app/app.py
@@ -1,3 +1,15 @@
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, app!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 # Entry point for the application.
-from . import app    # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes.
+# from . import app    # For application discovery by the 'flask' command.
+# from . import views  # For import side-effects of setting up routes.
