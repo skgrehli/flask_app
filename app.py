431ba2306a9e8a97f46b371c50f38e7bd669c4aa
Rickard Samuelsson <rickardsamuelsson@Rickards-MacBook-Pro.local>
2019-05-13 13:59:46 +0200

    Zip-file containing basic app for setup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return "Hello, world!"
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
