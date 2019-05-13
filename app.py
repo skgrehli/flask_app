9746ad302b1c6096b543c28cd66073cda3e2e641
Rickard Samuelsson <rickardsamuelsson@Rickards-MacBook-Pro.local>
2019-05-13 16:24:27 +0200

    Heroku fix

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-# Entry point for the application.
-from . import app    # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes.
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, world!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
