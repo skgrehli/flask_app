cac051a248242e15268856c01f6adc04fcc213af
Rickard Samuelsson <rickardsamuelsson@Rickards-MacBook-Pro.local>
2019-05-14 11:21:04 +0200

    New try

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,6 @@
-from flask import render_template
 from app import app
 
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     return render_template("index.html")
-
-@app.route("/")
-def home():
-    return "Hello, app!"
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
