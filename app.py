296b2eeac9af8b5d024991e1886b0cafbaf42072
Rickard Samuelsson <rickardsamuelsson@Rickards-MacBook-Pro.local>
2019-05-14 11:12:46 +0200

    App reset

--- app.py
+++ app.py
@@ -2,14 +2,10 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-# @app.route("/")
-# def home():
-#     return "Hello, app!"
+@app.route("/")
+def home():
+    return "Hello, world!"
 
-# if __name__ == '__main__':
-#     port = int(os.environ.get('PORT', 5000))
-#     app.run(host='0.0.0.0', port=port)
-
-# Entry point for the application.
-# from . import app    # For application discovery by the 'flask' command.
-# from . import views  # For import side-effects of setting up routes.
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
