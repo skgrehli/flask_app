c8dc1a26795fcda0ce211b0065c1b21218d3278a
Rickard Samuelsson <rickardsamuelsson@Rickards-MacBook-Pro.local>
2019-05-21 12:10:49 +0200

    Installed Feedparser and updated requirements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from app import app
 
-# app seems to break without following
+# app seems to break without following, consider adding ', debug=True' after 'port=port'
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
